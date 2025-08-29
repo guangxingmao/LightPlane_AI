3d632c552a70d67401ba1738ad179b4a1524cd25
shinxmao <14216606+shinxmao@user.noreply.gitee.com>
2025-08-29 12:31:24 +0800

    111

---
+++ cleanup_unused_files.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# 自动删除未使用的文件脚本
+
+echo "🗑️ 开始删除未使用的文件..."
+
+rm "analyze_unused_files.py"
+rm "train_and_integrate_ai.py"
+rm "backup_before_cleanup/rl_ai_controller.py"
+rm "backup_before_cleanup/monitor_training.py"
+rm "backup_before_cleanup/advanced_ai_matting.py"
+rm "backup_before_cleanup/complex_image_matting.py"
+rm "backup_before_cleanup/ai_generated_image_matting.py"
+rm "backup_before_cleanup/complex_image_matting_fixed.py"
+rm "backup_before_cleanup/check_training_status.py"
+rm "ai_controllers/check_training.py"
+rm "ai_controllers/train_ai_decision.py"
+rm "ai_controllers/realistic_ai_controller.py"
+rm "ai_controllers/train_ai.py"
+rm "ai_controllers/__init__.py"
+rm "ai_controllers/train_integrated_plane.py"
+rm "ai_controllers/train_game_strategy.py"
+
+echo "✅ 删除完成！"
